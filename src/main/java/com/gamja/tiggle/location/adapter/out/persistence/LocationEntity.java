f488ee5dcc585a15beb664ee2a9ca8720d211e17
postrel63 <postrel63@naver.com>
2024-07-17 16:41:53 +0900

    [Feat] Location 도메인 모델과 영속성 엔티티 추가

---
+++ src/main/java/com/gamja/tiggle/location/adapter/out/persistence/LocationEntity.java
@@ -0,0 +1,24 @@
+package com.gamja.tiggle.location.adapter.out.persistence;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class LocationEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+}
