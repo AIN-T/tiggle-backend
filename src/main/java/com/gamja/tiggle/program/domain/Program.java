0ea7c1dd3cb724e589758702ddd17cd7aa6979dd
postrel63 <postrel63@naver.com>
2024-07-17 19:08:11 +0900

    [Refactor] 도메인 Program에 BaseEntity 제거

--- src/main/java/com/gamja/tiggle/program/domain/Program.java
+++ src/main/java/com/gamja/tiggle/program/domain/Program.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Builder
 @Getter
-public class Program extends BaseEntity {
+public class Program{
     private Long id;
     private String programName;
     private String programInfo;
