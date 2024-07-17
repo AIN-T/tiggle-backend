04137b324dd5b4c752a8a92ea48e00d4144f2c9b
postrel63 <postrel63@naver.com>
2024-07-18 01:03:28 +0900

    [Feat] ReservationType 필드명 변경 #21
        AVAILABLE,  // 예약 가능
        IN_PROGRESS,  // 예약 중
        COMPLETED,  // 예약 완료
        REFUNDED, // 환불됨
        EXCHANGED, // 교환됨

--- src/main/java/com/gamja/tiggle/reservation/domain/type/ReservationType.java
+++ src/main/java/com/gamja/tiggle/reservation/domain/type/ReservationType.java
@@ -1,7 +1,10 @@
 package com.gamja.tiggle.reservation.domain.type;
 
 public enum ReservationType {
-    PENDING,  // 예약 전
+    AVAILABLE,  // 예약 가능
     IN_PROGRESS,  // 예약 중
-    COMPLETED  // 예약 완료
+    COMPLETED,  // 예약 완료
+    REFUNDED, // 환불됨
+    EXCHANGED, // 교환됨
+
 }
