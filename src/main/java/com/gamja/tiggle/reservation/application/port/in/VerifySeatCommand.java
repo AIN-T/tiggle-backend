0030c014f2a29d1fd6e451cb698b6f255e7d61d7
postrel63 <postrel63@naver.com>
2024-07-18 21:05:00 +0900

    [Feat] 요청 응답 DTO  #37

---
+++ src/main/java/com/gamja/tiggle/reservation/application/port/in/VerifySeatCommand.java
@@ -0,0 +1,14 @@
+package com.gamja.tiggle.reservation.application.port.in;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class VerifySeatCommand {
+
+    private Long seatId;
+    private Long userId;
+    private Long programId;
+    private Long timesId;
+}
