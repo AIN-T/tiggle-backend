52d8e5c7b1f475bd59524ba971bd7bfd0f277ab2
postrel63 <postrel63@naver.com>
2024-07-19 01:30:07 +0900

    [Feat] 엔티티 및 도메인 설정 #41

---
+++ src/main/java/com/gamja/tiggle/program/adapter/in/web/response/GetProgramDetailResponse.java
@@ -0,0 +1,22 @@
+package com.gamja.tiggle.program.adapter.in.web.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+public class GetProgramDetailResponse {
+
+    private String programName;
+    private Long locationId;
+    private LocalDateTime programStartDate;
+    private LocalDateTime programEndDate;
+    private int runtime;
+    private List<String> imageUrls;
+    private String programInfo;
+    private List<PriceResponse> pricesList;
+
+}
