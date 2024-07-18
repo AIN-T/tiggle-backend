0a2d22a910c917eb0f806dfd0e0dca820efe286f
yeon <dutls5367@gmail.com>
2024-07-18 09:45:14 +0900

    [Feat] Program 조회 시 응답 DTO 구현

---
+++ src/main/java/com/gamja/tiggle/program/adapter/in/web/response/ReadProgramResponse.java
@@ -0,0 +1,19 @@
+package com.gamja.tiggle.program.adapter.in.web.response;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@Getter
+public class ReadProgramResponse {
+    private String programName;
+    private Long categoryId;
+    private String programInfo;
+    private LocalDateTime programStartDate;
+    private LocalDateTime reservationOpenDate;
+    private LocalDateTime programEndDate;
+    private List<String> imageFiles;
+}
