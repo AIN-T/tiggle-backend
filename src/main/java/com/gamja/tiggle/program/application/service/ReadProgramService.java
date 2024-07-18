e85d386033fe4dc2e6d7841888ccc1a147d1bb93
yeon <dutls5367@gmail.com>
2024-07-18 09:46:10 +0900

    [Feat] Program 조회 Service 구현

---
+++ src/main/java/com/gamja/tiggle/program/application/service/ReadProgramService.java
@@ -0,0 +1,34 @@
+package com.gamja.tiggle.program.application.service;
+
+import com.gamja.tiggle.common.BaseException;
+import com.gamja.tiggle.common.annotation.UseCase;
+import com.gamja.tiggle.program.application.port.in.ReadProgramCommand;
+import com.gamja.tiggle.program.application.port.in.ReadProgramUseCase;
+import com.gamja.tiggle.program.application.port.out.ReadProgramPort;
+import com.gamja.tiggle.program.domain.Program;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+
+@UseCase
+@RequiredArgsConstructor
+public class ReadProgramService implements ReadProgramUseCase {
+    private final ReadProgramPort readProgramPort;
+
+    @Override
+    public List<Program> readProgramAll(ReadProgramCommand command) throws BaseException {
+        Program program = Program.builder()
+                .categoryId(command.getCategoryId())
+                .build();
+        return readProgramPort.readProgramAll(program);
+    }
+
+    @Override
+    public List<Program> readRealTimeAll(ReadProgramCommand command) throws BaseException {
+        Program program = Program.builder()
+                .currentDateTime(command.getCurrentDateTime())
+                .build();
+        return readProgramPort.readRealTimeAll(program.getCurrentDateTime());
+    }
+}
