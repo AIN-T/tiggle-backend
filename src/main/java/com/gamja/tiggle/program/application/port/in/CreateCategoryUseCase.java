d60ebae2cd95d8b98b25fff12d937ce767c1c541
yeon <dutls5367@gmail.com>
2024-07-18 16:34:35 +0900

    [Feat] 카테고리 에러코드 추가

--- src/main/java/com/gamja/tiggle/program/application/port/in/CreateCategoryUseCase.java
+++ src/main/java/com/gamja/tiggle/program/application/port/in/CreateCategoryUseCase.java
@@ -1,5 +1,7 @@
 package com.gamja.tiggle.program.application.port.in;
 
+import com.gamja.tiggle.common.BaseException;
+
 public interface CreateCategoryUseCase {
-    void createCategory(CreateCategoryCommand command);
+    void createCategory(CreateCategoryCommand command) throws BaseException;
 }
--- src/main/java/com/gamja/tiggle/program/application/service/CreateCategoryService.java
+++ src/main/java/com/gamja/tiggle/program/application/service/CreateCategoryService.java
@@ -1,5 +1,8 @@
 package com.gamja.tiggle.program.application.service;
 
+import com.gamja.tiggle.common.BaseException;
+import com.gamja.tiggle.common.BaseResponseStatus;
+import com.gamja.tiggle.program.adapter.out.persistence.JpaCategoryRepository;
 import lombok.RequiredArgsConstructor;
 import com.gamja.tiggle.program.application.port.in.CreateCategoryCommand;
 import com.gamja.tiggle.program.application.port.in.CreateCategoryUseCase;
@@ -11,12 +14,22 @@ import com.gamja.tiggle.common.annotation.UseCase;
 @RequiredArgsConstructor
 public class CreateCategoryService implements CreateCategoryUseCase {
     private final CreateCategoryPort createCategoryPort;
+    private final JpaCategoryRepository jpaCategoryRepository;
 
     @Override
-    public void createCategory(CreateCategoryCommand command) {
+    public void createCategory(CreateCategoryCommand command) throws BaseException {
+        // 예외 2. 중복된 카테고리 확인
+        if (jpaCategoryRepository.existsByCategoryName(command.getCategoryName())){
+            throw new BaseException(BaseResponseStatus.DUPLICATE_CATEGORY);
+        }
         Category category = Category.builder()
                 .categoryName(command.getCategoryName())
                 .build();
-        createCategoryPort.createCategory(category);
+        try {
+            createCategoryPort.createCategory(category);
+        } catch (BaseException e) {
+            throw new BaseException(BaseResponseStatus.CATEGORY_CREATION_FAILED);
+        }
+
     }
 }
