5df684e05df841e4c0bbbccc880308ab384fecd2
ashd89 <jyjhlee0809@gmail.com>
2024-07-19 02:41:21 +0900

    [feat] DB내 사용자 패스워드 암호화 및 로그인 사용자 토큰 발급 및 인가 구역 접속 #44

---
+++ src/main/java/com/gamja/tiggle/user/application/service/CustomUserDetailService.java
@@ -0,0 +1,36 @@
+package com.gamja.tiggle.user.application.service;
+
+
+import com.gamja.tiggle.user.adapter.out.persistence.JpaUserRepository;
+import com.gamja.tiggle.user.adapter.out.persistence.UserEntity;
+import com.gamja.tiggle.user.domain.CustomUserDetails;
+import com.gamja.tiggle.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+    private final JpaUserRepository jpaUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity result = jpaUserRepository.findByEmail(username);
+        User user = User.builder()
+                .email(result.getEmail())
+                .password(result.getPassword())
+                .role(result.getRole())
+                .enable(true)
+                .build();
+
+        if (result!=null) {
+            return new CustomUserDetails(user);
+        } else {
+            return null;
+        }
+
+    }
+}
