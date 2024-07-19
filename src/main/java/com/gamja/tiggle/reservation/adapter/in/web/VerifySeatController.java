f5cf6ae6b122edea88d6acd65986923393a0f09d
postrel63 <postrel63@naver.com>
2024-07-19 09:56:51 +0900

    [Docs] swagger #47

--- src/main/java/com/gamja/tiggle/reservation/adapter/in/web/VerifySeatController.java
+++ src/main/java/com/gamja/tiggle/reservation/adapter/in/web/VerifySeatController.java
@@ -7,6 +7,8 @@ import com.gamja.tiggle.common.annotation.WebAdapter;
 import com.gamja.tiggle.reservation.adapter.in.web.request.VerifySeatRequest;
 import com.gamja.tiggle.reservation.application.port.in.VerifySeatCommand;
 import com.gamja.tiggle.reservation.application.port.in.VerifySeatUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,11 +17,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @WebAdapter
 @RequiredArgsConstructor
 @RequestMapping("/verify")
+@Tag(name = "좌석검증 컨트롤러", description = "선택한 좌석이 예약 가능한지 검증")
 public class VerifySeatController {
 
     private final VerifySeatUseCase verifySeatUseCase;
 
     @PostMapping
+    @Operation(summary = "좌석 예약 검증")
     public BaseResponse<Void> verifySeatController(
             @RequestBody VerifySeatRequest request) {
 
