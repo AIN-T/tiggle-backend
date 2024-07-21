8003582e699702d14199da1ff5e2ca6b910265cd
yunseul <siddl333@naver.com>
2024-07-22 00:13:24 +0900

    [fix] 매개변수 클래스타입 수정

--- src/main/java/com/gamja/tiggle/reservation/adapter/out/persistence/ReadReservationPersistenceAdapter.java
+++ src/main/java/com/gamja/tiggle/reservation/adapter/out/persistence/ReadReservationPersistenceAdapter.java
@@ -6,7 +6,6 @@ import com.gamja.tiggle.common.annotation.PersistenceAdapter;
 import com.gamja.tiggle.reservation.adapter.out.persistence.Entity.ReservationEntity;
 import com.gamja.tiggle.reservation.adapter.out.persistence.repositroy.ReservationRepository;
 import com.gamja.tiggle.reservation.application.port.out.ReadReservationPort;
-import com.gamja.tiggle.reservation.domain.Reservation;
 import com.gamja.tiggle.user.domain.User;
 import lombok.RequiredArgsConstructor;
 
@@ -20,8 +19,8 @@ public class ReadReservationPersistenceAdapter implements ReadReservationPort {
     private final ReservationRepository repository;
 
     @Override
-    public ReservationEntity read(Reservation reservation) throws BaseException {
-        return repository.findById(reservation.getId()).orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_RESERVATION));
+    public ReservationEntity read(Long reservationId) throws BaseException {
+        return repository.findById(reservationId).orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_RESERVATION));
     }
 
     @Override
--- src/main/java/com/gamja/tiggle/reservation/application/port/out/ReadReservationPort.java
+++ src/main/java/com/gamja/tiggle/reservation/application/port/out/ReadReservationPort.java
@@ -2,12 +2,11 @@ package com.gamja.tiggle.reservation.application.port.out;
 
 import com.gamja.tiggle.common.BaseException;
 import com.gamja.tiggle.reservation.adapter.out.persistence.Entity.ReservationEntity;
-import com.gamja.tiggle.reservation.domain.Reservation;
 import com.gamja.tiggle.user.domain.User;
 
 import java.util.List;
 
 public interface ReadReservationPort {
-    ReservationEntity read(Reservation reservation) throws BaseException;
+    ReservationEntity read(Long reservationId) throws BaseException;
     List<ReservationEntity> readExchangeOfferForMe(User user);
 }
