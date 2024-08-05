9c3cff0c1eaad29233e3689fbfdeab78673d5e04
yeon <dutls5367@gmail.com>
2024-08-05 14:23:09 +0900

    [Refactor] 상세 정보 값 수정 및 import 정리

--- src/main/java/com/gamja/tiggle/reservation/domain/Reservation.java
+++ src/main/java/com/gamja/tiggle/reservation/domain/Reservation.java
@@ -1,7 +1,6 @@
 package com.gamja.tiggle.reservation.domain;
 
 import com.gamja.tiggle.reservation.domain.type.ReservationType;
-import com.gamja.tiggle.user.domain.User;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.Builder;
@@ -33,9 +32,10 @@ public class Reservation {
 
     // 예매 받아올 값
     private LocalDateTime createdAt; // 예약 날짜
-    private LocalDateTime programStartDate; // 공연 날짜
+    private LocalDateTime date; // 공연 날짜
     private String locationName; // 공연장
     private String name; // 예매자
     private String seatInfo; // 좌석 정보
+    private String gradeName; // 좌석 등급
 
 }
