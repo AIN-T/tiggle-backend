3999cb651c6b2135abe9a8b1995ae81c2d7ece7a
yeon <dutls5367@gmail.com>
2024-08-05 17:18:20 +0900

    [Feat] 내가 예매한 전체 티켓 목록 받아오기

--- src/main/java/com/gamja/tiggle/reservation/adapter/out/persistence/repositroy/ReservationRepository.java
+++ src/main/java/com/gamja/tiggle/reservation/adapter/out/persistence/repositroy/ReservationRepository.java
@@ -1,6 +1,8 @@
 package com.gamja.tiggle.reservation.adapter.out.persistence.repositroy;
 
 import com.gamja.tiggle.reservation.adapter.out.persistence.Entity.ReservationEntity;
+import com.gamja.tiggle.user.adapter.out.persistence.UserEntity;
+import com.gamja.tiggle.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -19,4 +21,11 @@ public interface ReservationRepository extends JpaRepository<ReservationEntity, 
             "JOIN FETCH r.user u " +
             "WHERE r.id = :reservationId")
     ReservationEntity findReservationWithDetails(@Param("reservationId") Long reservationId);
+
+    @Query("SELECT r FROM ReservationEntity r " +
+            "JOIN FETCH r.seatEntity s " +
+            "JOIN FETCH r.programEntity p " +
+            "JOIN FETCH r.user u " +
+            "WHERE u.id = :userId")
+    List<ReservationEntity> findReservationsByUserId(@Param("userId") Long userId);
 }
