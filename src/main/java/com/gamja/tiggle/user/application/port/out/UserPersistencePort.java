3e969dd67f02ec4c97da6ee7eab40b2e0dd05050
ashd89 <jyjhlee0809@gmail.com>
2024-08-05 15:07:30 +0900

    [Feat] my-page내 사용자 정보 조회 기능 api #104

--- src/main/java/com/gamja/tiggle/user/application/port/out/UserPersistencePort.java
+++ src/main/java/com/gamja/tiggle/user/application/port/out/UserPersistencePort.java
@@ -1,6 +1,7 @@
 package com.gamja.tiggle.user.application.port.out;
 
 import com.gamja.tiggle.common.BaseException;
+import com.gamja.tiggle.user.adapter.out.persistence.UserEntity;
 import com.gamja.tiggle.user.domain.User;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +10,7 @@ public interface UserPersistencePort {
     void saveUser(User user) throws BaseException;
 
     void verifyUser(String email) throws BaseException;
+
+    User searchUser(Long id) throws BaseException;
 }
 
