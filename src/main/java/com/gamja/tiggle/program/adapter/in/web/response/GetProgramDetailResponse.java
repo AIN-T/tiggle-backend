32572b2321841b3e85f86c717b1e747522d92a52
postrel63 <postrel63@naver.com>
2024-08-05 20:12:23 +0900

    [refactor] 날짜 타입 맞추기 #112

--- src/main/java/com/gamja/tiggle/program/adapter/in/web/response/GetProgramDetailResponse.java
+++ src/main/java/com/gamja/tiggle/program/adapter/in/web/response/GetProgramDetailResponse.java
@@ -1,5 +1,6 @@
 package com.gamja.tiggle.program.adapter.in.web.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,8 +13,11 @@ public class GetProgramDetailResponse {
 
     private String programName;
     private Long locationId;
+    private String locationName;
     private int age;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private LocalDateTime programStartDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private LocalDateTime programEndDate;
     private int runtime;
     private List<String> imageUrls;
