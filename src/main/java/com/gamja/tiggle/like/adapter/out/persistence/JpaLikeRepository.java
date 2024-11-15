65e57c892a3b802e31363d945723172a76bfd1fb
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2024-11-15 19:50:30 +0900

    [Feat] 로그인한 유저의 좋아요 목록 조회 jpql 쿼리 구현

--- src/main/java/com/gamja/tiggle/like/adapter/out/persistence/JpaLikeRepository.java
+++ src/main/java/com/gamja/tiggle/like/adapter/out/persistence/JpaLikeRepository.java
@@ -3,9 +3,20 @@ package com.gamja.tiggle.like.adapter.out.persistence;
 import com.gamja.tiggle.program.adapter.out.persistence.Entity.ProgramEntity;
 import com.gamja.tiggle.user.adapter.out.persistence.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface JpaLikeRepository extends JpaRepository<LikeEntity, Long> {
     boolean existsByUserEntityAndProgramEntity(UserEntity userEntity, ProgramEntity programEntity);
 
     void deleteByUserEntityAndProgramEntity(UserEntity userEntity, ProgramEntity programEntity);
+
+
+    @Query("SELECT l FROM LikeEntity l " +
+            "JOIN FETCH l.programEntity p " +
+            "JOIN FETCH p.locationEntity loc " +
+            "WHERE l.userEntity.id = :userId")
+    List<LikeEntity> findLikesWithProgramAndLocationByUserId(@Param("userId") Long userId);
 }
