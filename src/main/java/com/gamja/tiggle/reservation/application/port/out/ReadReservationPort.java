70ab5838745b565a79eac36b9b9c54123d3679d8
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2024-11-22 22:09:44 +0900

    [Refactor] 데이터 조회 로직을 DB에서 Redis로 변경

--- src/main/java/com/gamja/tiggle/reservation/application/port/out/ReadReservationPort.java
+++ src/main/java/com/gamja/tiggle/reservation/application/port/out/ReadReservationPort.java
@@ -3,6 +3,7 @@ import com.gamja.tiggle.common.BaseException;
 import com.gamja.tiggle.reservation.adapter.out.persistence.Entity.ReservationEntity;
 import com.gamja.tiggle.reservation.application.port.in.ReadReservationCommand;
 import com.gamja.tiggle.reservation.domain.Reservation;
+
 import java.util.List;
 
 
@@ -11,5 +12,5 @@ public interface ReadReservationPort {
     Reservation readReservation(Reservation reservation) throws  BaseException;
     List<Reservation> myRead(ReadReservationCommand command) throws BaseException;
     Long getReservationCnt(Long userid) throws BaseException;
-    Reservation readTemporaryReservation(Reservation reservation);
+    Reservation readTemporaryReservation(Reservation reservation) throws BaseException;
 }
